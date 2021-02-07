3fcfd4ce41cb78bab7c4cf46da28ae86973e5b18
woongs <ccw1021@naver.com>
2021-02-07 12:27:21 +0900

    카드 12개 생성 및 tapGesture 추가

--- playingCard/playingCard/ViewController.swift
+++ playingCard/playingCard/ViewController.swift
@@ -12,38 +12,48 @@ class ViewController: UIViewController {
     
     var deck = PlayingCardDeck()
     
-    @IBOutlet weak var playingCardView: PlayingCardView! {
-        didSet {
-            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-            swipe.direction = [.left,.right]
-            playingCardView.addGestureRecognizer(swipe)
-            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
-            playingCardView.addGestureRecognizer(pinch)
-        }
-    }
+    @IBOutlet var playingCardViews: [PlayingCardView]!
     
-    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
-        switch sender.state {
-        case .ended:
-            playingCardView.isFaceUp = !playingCardView.isFaceUp
-        default:
-            break
-        }
-    }
-    
-    @objc func nextCard() {
-        if let card = deck.draw() {
-            playingCardView.rank = card.rank.order
-            playingCardView.suit = card.suit.rawValue
+    @objc func nextCard(_ recognizer: UISwipeGestureRecognizer) {
+        if let chosenCardView = recognizer.view as? PlayingCardView, let card = deck.draw() {
+            chosenCardView.rank = card.rank.order
+            chosenCardView.suit = card.suit.rawValue
         }
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var cards = [PlayingCard]()
+        for _ in 1...((playingCardViews.count+1)/2) {
+            let card = deck.draw()!
+            cards += [card, card]
+        }
+        for cardView in playingCardViews {
+            cardView.isFaceUp = false
+            let card = cards.remove(at: cards.count.arc4random)
+            cardView.rank = card.rank.order
+            cardView.suit = card.suit.rawValue
+            let tap = UITapGestureRecognizer(target: self, action: #selector(flipCard(_:)))
+            cardView.addGestureRecognizer(tap)
+        }
     }
 
-
+    @objc func flipCard(_ recognizer: UITapGestureRecognizer) {
+        switch recognizer.state {
+        case .ended:
+            guard let chosenCardView = recognizer.view as? PlayingCardView else { return }
+            UIView.transition(
+                with: chosenCardView,
+                duration: 0.6,
+                options: [.transitionFlipFromLeft],
+                animations: {
+                    chosenCardView.isFaceUp = !chosenCardView.isFaceUp
+                }
+            )
+        default:
+            break
+        }
+    }
 }
 
